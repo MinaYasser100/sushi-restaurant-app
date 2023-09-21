f1f87b4113bcac0ff38f6a54d11b14aef7f977a0
MinaYasser100 <minayasser43@gmail.com>
2023-09-21 10:32:50 +0300

    food model

--- lib/features/menu/views/widgets/menu_view_body.dart
+++ lib/features/menu/views/widgets/menu_view_body.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:sushi_restaurant_app/core/model/food_model.dart';
+import 'package:sushi_restaurant_app/core/utils/constant.dart';
+import 'package:sushi_restaurant_app/core/utils/styles.dart';
 import '../../../../core/function/custom_outline_input_border.dart';
 import 'promo_banner_menu.dart';
 
-class MenuViewBody extends StatelessWidget {
+class MenuViewBody extends StatefulWidget {
   const MenuViewBody({super.key});
+
+  @override
+  State<MenuViewBody> createState() => _MenuViewBodyState();
+}
+
+class _MenuViewBodyState extends State<MenuViewBody> {
+  List<FoodModel> foodMenu = [
+    FoodModel(
+      name: 'Slamon Sushi',
+      price: '21.00',
+      imagePath: 'assets/images/one.png',
+      rating: '4.9',
+    ),
+    FoodModel(
+      name: 'Tuna',
+      price: '23.00',
+      imagePath: 'assets/images/three.png',
+      rating: '4.3',
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const PromoBannerMenu(),
         const SizedBox(
@@ -24,6 +48,16 @@ class MenuViewBody extends StatelessWidget {
         ),
         const SizedBox(
           height: 25,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Text(
+            'Food Menu',
+            style: Styles.textStyle18.copyWith(
+              fontWeight: FontWeight.bold,
+              color: kDarkColor,
+            ),
+          ),
         ),
       ],
     );
